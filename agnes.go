343b4af63f669c5ab77744fe663457c35c3d41fc
jinseokYeom <jinyeom95@gmail.com>
2015-10-10 19:00:01 -0500

    progress

--- agnes.go
+++ agnes.go
@@ -5,11 +5,6 @@ import ("fmt"
         "os"
         "os/exec"
         "math"
-        // "log"
-        // "net/smtp"
-        // "bytes"
-        // "text/template"
-        // "strconv"
 )
 
 // Agnes logo in small case
@@ -20,55 +15,65 @@ const AGNES_LOGO string =
        ##         ###      ##       #        ##       #      ##         ##
        ##         ###       ##    #           ##     #       ##         ##
        ##         ###        ##  #             ##   #        ##         ##
-        ##       ## ##        ###               ## #         ##         ##
-          #######    ###      ##      AGNES      ##          ##         ##
+        ##       ## ##        ###   AGNES I/O   ## #         ##         ##
+          #######    ###      ##     v.0.1.0     ##          ##         ##
                              ###                                        ##
                              ##                                         ##
 `
 
 // global constants
-I_LAYER = 784           // INPUT_LAYER: size of a box (784 = 28 * 28)
-H_LAYER_1 = 100         // HIDDEN LAYER 1
-H_LAYER_2 = 100         // HIDDEN LAYER 2
-H_LAYER_3 = 100         // HIDDEN LAYER 3
-O_LAYER = 10            // OUTPUT_LAYER:
+const (
+    I_LAYER = 784   // INPUT_LAYER: 784 = 28 * 28
+    H_LAYER = 15    // HIDDEN LAYER: 15 neurons
+    O_LAYER = 10    // OUTPUT_LAYER: from 0 to 9
+)
 
 // define a Neural Network
-type NeuralNetwork []Layer
-// define a neuron layer
-type Layer []Neuron
+type NeuralNetwork struct {
+    inputLayer []*Neuron    // input layer
+    hiddenLayer []*Neuron   // hidden layer
+    outputLayer []*Neuron   // output layer
+}
+
 // define a Neuron
 type Neuron struct {
-    input float64
-    output float64
+    input float64   // sum of previous layer's outputs
+    output float64  // result of sigmoid function
 }
 
-// generate and initialize a neural network
-func genNeuralNet() *NeuralNetwork {
-
+// genrate a neural network
+func genNeuralNetwork() *NeuralNetwork {
+    var i []*Neuron = genLayer(I_LAYER)     // input layer
+    var h []*Neuron = genLayer(H_LAYER)     // hidden layer
+    var o []*Neuron = genLayer(O_LAYER)     // output layer
+    return &NeuralNetwork{i, h, o}
 }
 
-// generate and initialize a neuron layer
-func genLayer() *Layer {
-
+// generate a neural layer
+func genLayer(numNeuron int) []*Neuron {
+    var layer []*Neuron = make([]*Neuron, numNeuron)
+    for i := 0; i < numNeuron; i++ { layer[i] = genNeuron() }
+    return layer
 }
 
 // generate and initialize a neuron
-func genNeuron() *Neuron {
-
-}
+func genNeuron() *Neuron { return &Neuron{0.0, 0.0} }
 
 // Sigmoid function: choose between 0 and 1 based on input value
-func sigmoid(x float64) float64 {
-    // sigmoid(0.0) = 0.5
-    return 1.0 / (1.0 + math.Pow(math.E, -x))
+func sigmoid(x float64) float64 { return 1.0 / (1.0 + math.Pow(math.E, -x)) }
+
+// read and analyze a scanned image of hand-written numbers
+func getImageData() {
+    // input file
+    // create data
+    // return data
 }
 
 // feedforward neural network where information flows only one way
 func feedforwardNeuralNet() {
-    // input file
-    // read data
-    brain := genNeuralNet()
+    getImageData()                  // input file and get data
+    brain := genNeuralNetwork()     // generate a brain
+
 }
 
 // clear the terminal screen
