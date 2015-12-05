a75b2571a07dd6d98268886f0b323f8598347119
jinseokYeom <jinyeom95@gmail.com>
2015-12-05 15:52:52 -0600

    progress

--- agnes.go
+++ agnes.go
@@ -7,6 +7,7 @@ import ("fmt"
         "math"
         "image"
         "image/jpeg"
+        "image/color"
 )
 
 // Agnes logo in small case
@@ -17,7 +18,7 @@ const AGNES_LOGO string =
        ##         ###      ##       #        ##       #      ##         ##
        ##         ###       ##    #           ##     #       ##         ##
        ##         ###        ##  #             ##   #        ##         ##
-        ##       ## ##        ###   AGNES I/O   ## #         ##         ##
+        ##       ## ##        ###     AGNES     ## #         ##         ##
           #######    ###      ##     v.0.1.0     ##          ##         ##
                              ###                                        ##
                              ##                                         ##
@@ -33,49 +34,92 @@ const (
 )
 
 // define a Neural Network
-type NeuralNetwork struct {
-    inputLayer []*Neuron    // input layer
-    hiddenLayer []*Neuron   // hidden layer
-    outputLayer []*Neuron   // output layer
+type NNetwork struct {
+    numInputs       int             // number of inputs
+    numOutputs      int             // number of outputs
+    numHNeurons     int             // number of neurons in each hidden layer
+    numHLayers      int             // number of hiddne layers
+    layers          []*NLayer       // slice of layers
+}
+
+func NewNNetwork(nIns, nOuts, nHlNs, nHls int) *NNetwork {
+    var ls []*NLayer
+    // if there are hidden layers,
+    if nHls > 0 {
+        ls = make([]*NLayer, nHls)
+        // first hidden layer
+        ls[0] = NewNLayer(nHlNs, nIns)
+        // mid hidden layers
+        for i := 1; i < nHls - 1; i++ { ls[i] = NewNLayer(nHlNs, nHlNs) }
+        // output layer
+        ls[nHls - 1] = NewNLayer(nOuts, nHlNs)
+    } else {
+        ls = make([]*NLayer, 1)
+        ls[0] = NewNLayer(nOuts, nIns)
+    }
+    return &NNetwork{nIns, nOuts, nHlNs, nHls, ls}
+}
+
+func (n *NNetwork) GetWeights() *list.List {
+    weights := list.New()
+    for i := range n.layers {
+    for j := range n.layers[i].neurons {
+    for k := range n.layers[i].neurons[j].weights {
+        weights.PushBack(n.layers[i].neurons[j].weights[k])
+    } } }
+    return weights
+}
+
+func (n *NNetwork) SetWeights(newWeights []float64) {
+    cWeight := 0
+    for i := range n.layers {
+    for j := range n.layers[i].neurons {
+    for k := range n.layers[i].neurons[j].weights {
+        n.layers[i].neurons[j].weights[k] = newWeights[cWeight]
+        cWeight++
+    } } }
+}
+
+func (n * NNetwork) Update() {
+
+}
+
+func Sigmoid(x float64) float64 {
+    return 1.0 / (1.0 + math.Pow(math.E, -x))
+}
+
+// define a Neuron Layer
+type NLayer struct {
+    numNeurons      int
+    neurons         []*Neuron
+}
+
+func NewNLayer(nNrns, nIns int) *NLayer {
+    ns := make([]*Neuron, nNrns)
+    for i := range ns { ns[i] = NewNeuron(nIns) }
+    return &NLayer{nNrns, ns}
 }
 
 // define a Neuron
 type Neuron struct {
-    i float64   // sum of previous layer's outputs
-    o float64   // result of sigmoid function
+    numInputs       int
+    weights         []float64
 }
 
-// genrate a neural network
-func genNeuralNetwork() *NeuralNetwork {
-    var i []*Neuron = genLayer(I_LAYER)     // input layer
-    var h []*Neuron = genLayer(H_LAYER)     // hidden layer
-    var o []*Neuron = genLayer(O_LAYER)     // output layer
-    return &NeuralNetwork{i, h, o}
+// **** change the pararmeter to DNA! ****
+func NewNeuron(nIns int) *Neuron {
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+    w := make([]float64, nIns + 1)
+    for i := range w { w[i] = r.Float64() }
+    return &Neuron{nIns, w}
 }
 
-// generate a neural layer
-func genLayer(numNeuron int) []*Neuron {
-    var layer []*Neuron = make([]*Neuron, numNeuron)
-    for i := 0; i < numNeuron; i++ { layer[i] = genNeuron() }
-    return layer
+func (n *Neuron) output(inputs []float64) float64 {
+    var sum float64 = 0
+    for i := range inputs { sum += inputs[i] * n.weights[i] }
+    return sum
 }
 
-// generate and initialize a neuron
-func genNeuron() *Neuron { return &Neuron{0.0, 0.0} }
-
-// Sigmoid function: choose between 0 and 1 based on input value
-func sigmoid(x float64) float64 { return 1.0 / (1.0 + math.Pow(math.E, -x)) }
-
-// create an image
-func test() {
-
-	img := image.NewRGBA(image.Rect(1, 1, S_IMG, S_IMG))
-
-    newImage, _ := os.Create("new.jpg")
-    defer newImage.Close()
-
-    jpeg.Encode(newImage, img, &jpeg.Options{jpeg.DefaultQuality})
-}
 
 // clear the terminal screen
 func clear() {
@@ -92,14 +136,9 @@ func intro() {
     clear()                         // clear the terminal screen
 }
 
-// feedforward neural network where information flows only one way
-func feedforwardNeuralNet() {
-    // brain := genNeuralNetwork()             // generate a brain
-}
-
 // Agnes main function
 func main() {
     intro()                 // print intro
-    test()
+    testImage()
     // feedforwardNeuralNet()
 }
